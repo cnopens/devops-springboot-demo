d4f0276d16268b2fd1767f48b05c746cda725eef
cnopens <cnopens@163.com>
2020-09-16 17:43:40 +0800

    first commit

---
+++ src/main/java/com/cnopens/devopsspringbootdemo/DevopsSpringbootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cnopens.devopsspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsSpringbootDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cnopens/devopsspringbootdemo/DevopsSpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnopens.devopsspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsSpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
